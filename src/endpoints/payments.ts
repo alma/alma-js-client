3d75c991077befd725edd0ff0357540ac056caf7
Olivier Lance <olivier.lance@getalma.eu>
2020-01-08 14:16:49 +0100

    Update Eligibility endpoint to support multiple installments counts responses

--- src/endpoints/payments.ts
+++ src/endpoints/payments.ts
@@ -5,9 +5,26 @@ import Eligibility from "../entities/eligibility";
 const PAYMENTS_PATH = "/v1/payments";
 
 export class PaymentsEndpoint extends Endpoint {
-  async eligibility(data: PaymentEligibilityPayload): Promise<Eligibility> {
+  async eligibility(data: PaymentEligibilityPayload): Promise<Eligibility | Eligibility[]> {
     let response = await this.request(`${PAYMENTS_PATH}/eligibility`).setBody(data).post();
-    return new Eligibility(response.data)
+
+    let serverError = response.statusCode >= 500;
+    let responseData = response.data;
+    let result: Eligibility | Eligibility[];
+
+    if (!serverError && Array.isArray(responseData)) {
+      responseData = responseData.sort((a, b) => {
+        return a.installments_count - b.installments_count;
+      });
+
+      result = responseData.map((d: any) => new Eligibility(d));
+    } else if (!serverError && responseData && typeof responseData === "object") {
+      result = new Eligibility(responseData);
+    } else {
+      result = new Eligibility({eligible: false});
+    }
+
+    return result;
   }
 }
 
